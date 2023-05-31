d4103958ffa3a697229c7213b0decfa56f608403
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-05-30 23:46:32 -0300

    feat: create entities state, city and address

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { UserEntity } from './interfaces/user.entity';
+import { UserEntity } from './entities/user.entity';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { hash } from 'bcrypt';
 import { InjectRepository } from '@nestjs/typeorm';
